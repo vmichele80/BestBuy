2e18a79726e306345094c2e8456b1c3ac17f407d
Mike <vmichele80@hotmail.com>
2026-02-13 16:02:47 +0100

    added skeleton for store.py class

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from products import Product
+from store import Store
 
+product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                Product("Google Pixel 7", price=500, quantity=250),
+               ]
 
+best_buy = Store(product_list)
+
+for product in best_buy.product_list:
+    product.show()
+
+"""
 bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
 mac = Product("MacBook Air M2", price=1450, quantity=100)
 
@@ -9,3 +20,4 @@ print(bose.get_quantity())
 print(f"${bose.buy(2)}")
 
 print(bose.get_quantity())
+"""
---
+++ store.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from products import Product
+
+class Store:
+
+    def __init__(self, product_list: list):
+        """it initilises an istance of a store"""
+        self.product_list = product_list
+
+    def add_product(self, product):
+        """it adds a product to the store"""
+        self.product_list.append(product)
+
+    def remove_product(self, product):
+        """it removes a product from the store"""
+        self.product_list.remove(product)
+
+    def get_total_quantity(self) -> int:
+        """Returns how many items are in the store in total."""
+        pass
+
+    def get_all_products(self) -> list[Product]:
+        """Returns all products in the store that are active."""
+        pass
+
+    def order(self, shopping_list) -> float:
+        """Gets a list of tuples, where each tuple has 2 items:
+        Product (Product class) and quantity (int).
+        Buys the products and returns the total price of the order."""
+        pass
